db788031a4363380b2488097564ed98d55855a5a
Gabriel Noah <gabrieljnoah@gmail.com>
2025-03-01 13:47:58 -0600

    removed angry comment

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,7 +2,6 @@ return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     config = function ()
-        -- No fucking clue if this is running
         local configs = require("nvim-treesitter.configs")
         configs.setup({
             ensure_installed = { "lua", "python", "c", "cpp", "make", "javascript", "java", "rust" },
