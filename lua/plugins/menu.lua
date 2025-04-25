55f156e94864f3327330c90937ff27ef4364ad6d
Gabriel Noah <gabrieljnoah@gmail.com>
2025-04-24 19:42:04 -0500

    Forgot to commit this apparently

---
+++ lua/plugins/menu.lua
@@ -0,0 +1,9 @@
+return {
+    "nvzone/menu",
+    dependencies = { "nvzone/volt" },
+    config = function ()
+        vim.keymap.set("n", "<leader>t", function()
+            require("menu").open("default")
+        end, {})
+    end
+}
