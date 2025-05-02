0f4bab2fe54d755418b72faba08b668dbcc9c000
Gabriel Noah <gabrieljnoah@gmail.com>
2025-05-01 22:32:02 -0500

    added neoscoll plugin

---
+++ lua/plugins/neoscroll.lua
@@ -0,0 +1,15 @@
+return {
+    "karb94/neoscroll.nvim",
+    config = function()
+        require("neoscroll").setup({
+            mappings = {
+                "<C-d>", "<C-u>", "<C-f>", "<C-b>", "zz"
+            },
+            hide_cursor = false,
+            duration_multiplier = 0.9,
+            ignored_events = {
+                "WinScrolled", "CursorMoved"
+            }
+        })
+    end
+}
