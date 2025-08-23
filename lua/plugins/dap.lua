6555088f946d66b3507e737ef8d057e978e6c5d5
Gabriel Noah <gabrieljnoah@gmail.com>
2025-08-23 10:19:20 -0500

    Set up debugging with python

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -1,20 +1,30 @@
 return {
-    "mfussenegger/nvim-dap",
-    dependencies = { "rcarriga/nvim-dap-ui", "nvim-neotest/nvim-nio" },
-    config = function()
-        local dap = require("dap")
+    {
+        "mfussenegger/nvim-dap",
+        dependencies = { "rcarriga/nvim-dap-ui", "nvim-neotest/nvim-nio" },
+        config = function()
+            local dap = require("dap")
+            require("dapui").setup()
 
-        -- Keybinds
-        vim.keymap.set("n", "<F5>", dap.continue)
-        vim.keymap.set("n", "<F10>", dap.step_over)
-        vim.keymap.set("n", "<F11>", dap.step_into)
-        vim.keymap.set("n", "<F12>", dap.step_out)
-        vim.keymap.set("n", "<Leader>b", dap.toggle_breakpoint)
-        vim.keymap.set({ "n", "v" }, "<Leader>dh", function()
-            require("dap.ui.widgets").hover()
-        end)
-        vim.keymap.set({ "n", "v" }, "<Leader>dp", function()
-            require("dap.ui.widgets").preview()
-        end)
-    end,
+            -- Keybinds
+            vim.keymap.set("n", "<F5>", dap.continue)
+            vim.keymap.set("n", "<F10>", dap.step_over)
+            vim.keymap.set("n", "<F11>", dap.step_into)
+            vim.keymap.set("n", "<F12>", dap.step_out)
+            vim.keymap.set("n", "<Leader>b", dap.toggle_breakpoint)
+            vim.keymap.set({ "n", "v" }, "<Leader>dh", function()
+                require("dap.ui.widgets").hover()
+            end)
+            vim.keymap.set({ "n", "v" }, "<Leader>dp", function()
+                require("dap.ui.widgets").preview()
+            end)
+        end,
+    },
+    {
+        "mfussenegger/nvim-dap-python",
+        config = function ()
+            -- requires debugpy to be installed globally and executable with python3 -m debugpy
+            require("dap-python").setup("python3")
+        end
+    }
 }
