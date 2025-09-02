a2b9a67b1eb9af98f6912351a5e7e2c5a4f017b6
Gabriel Noah <gabrieljnoah@gmail.com>
2025-09-02 12:53:07 -0500

    disabled mini.pairs

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -4,7 +4,7 @@ return {
         require("mini.ai").setup()
         require("mini.comment").setup()
         require("mini.move").setup()
-        require("mini.pairs").setup()
+        -- require("mini.pairs").setup()
         require("mini.splitjoin").setup()
         require("mini.surround").setup()
     end,
