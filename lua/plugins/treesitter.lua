80bca2fce0a000c3ab6aa30ee63db03decbe0e04
Gabriel Noah <gabrieljnoah@gmail.com>
2025-11-12 14:03:38 -0600

    Removed treesitter ensure installed (just install them yourself)

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,18 +4,6 @@ return {
     config = function()
         local configs = require("nvim-treesitter.configs")
         configs.setup({
-            ensure_installed = {
-                "lua",
-                "python",
-                "c",
-                "cpp",
-                "make",
-                "javascript",
-                "java",
-                "rust",
-                "typescript",
-                "html",
-            },
             highlight = { enable = true },
             indent = { enable = true },
             rainbow = {
