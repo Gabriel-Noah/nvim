7c75f8e9bbeb5a0c843db54a41920405d87eda45
Gabriel Noah <gabrieljnoah@gmail.com>
2025-12-05 15:59:11 -0600

    Added lazy loading to nvim-cmp

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -1,5 +1,6 @@
 return {
     "hrsh7th/nvim-cmp",
+    event = "InsertEnter",
     dependencies = { "hrsh7th/cmp-buffer", "hrsh7th/cmp-path", "hrsh7th/cmp-nvim-lsp" },
     config = function()
         local cmp = require("cmp")
