21102f12b6c4198e8f6eec797caefd5974f588b3
Gabriel Noah <gabrieljnoah@gmail.com>
2026-02-06 10:18:45 -0600

    Changed something with vim-tmux-nav

---
+++ lua/plugins/vim-tmux-navigator.lua
@@ -0,0 +1,17 @@
+return {
+    "christoomey/vim-tmux-navigator",
+    cmd = {
+        "TmuxNavigateLeft",
+        "TmuxNavigateDown",
+        "TmuxNavigateUp",
+        "TmuxNavigateRight",
+        "TmuxNavigatePrevious",
+        "TmuxNavigatorProcessList",
+    },
+    keys = {
+        { "<m-h>", "<cmd>TmuxNavigateLeft<cr>" },
+        { "<m-j>", "<cmd>TmuxNavigateDown<cr>" },
+        { "<m-k>", "<cmd>TmuxNavigateUp<cr>" },
+        { "<m-l>", "<cmd>TmuxNavigateRight<cr>" },
+    },
+}
