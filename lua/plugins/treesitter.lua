1f863cc97995bef61db0016d55d03b4ec3a42784
Gabriel Noah <gabrieljnoah@gmail.com>
2026-02-16 12:40:31 -0600

    Disabled tree sitter indentation in typst files

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,3 +1,4 @@
+---@diagnostic disable: missing-fields
 return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
@@ -10,5 +11,7 @@ return {
                 enable = true,
             },
         })
+        -- Disables treesitter indentation in typst files
+        vim.api.nvim_create_autocmd("BufEnter", { pattern = "*.typ", command = "TSDisable indent" })
     end,
 }
