ff6c13ed30dcab08f8b5a2dd9e4d2f655fc29c96
Gabriel Noah <gabrieljnoah@gmail.com>
2026-02-16 12:55:18 -0600

    Changed vim tmux plugin

--- lua/plugins/vim-tmux-navigator.lua
+++ lua/plugins/vim-tmux-navigator.lua
@@ -1,17 +1,13 @@
 return {
-    "christoomey/vim-tmux-navigator",
-    cmd = {
-        "TmuxNavigateLeft",
-        "TmuxNavigateDown",
-        "TmuxNavigateUp",
-        "TmuxNavigateRight",
-        "TmuxNavigatePrevious",
-        "TmuxNavigatorProcessList",
-    },
-    keys = {
-        { "<m-h>", "<cmd>TmuxNavigateLeft<cr>" },
-        { "<m-j>", "<cmd>TmuxNavigateDown<cr>" },
-        { "<m-k>", "<cmd>TmuxNavigateUp<cr>" },
-        { "<m-l>", "<cmd>TmuxNavigateRight<cr>" },
-    },
+    "alexghergh/nvim-tmux-navigation",
+    config = function ()
+        require("nvim-tmux-navigation").setup({
+            keybindings = {
+                left = "<M-h>",
+                down = "<M-j>",
+                up = "<M-k>",
+                right = "<M-l>",
+            }
+        })
+    end
 }
